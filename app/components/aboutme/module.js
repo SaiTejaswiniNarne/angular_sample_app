c30f47fc11dbe43be665edc5c625a25043afa8bb
Sai Tejaswini <S533726@nwmissouri.edu>
2019-05-22 14:36:51 -0500

    commit aboutme

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,19 @@
+let myapp= angular.module("Myapp",[]);
+
+let mycontroller = function($scope){
+    $scope.message="Hello World";
+}
+
+myapp.controller("MyController",mycontroller);
+
+let JSONController = function($scope){
+    let Student={
+        FirstName : "Sai",
+        LastName : "Narne",
+        Email : "S533726@nwmissouri.edu"
+    }
+
+    $scope.Student=Student;
+}
+
+myapp.controller("Student",JSONController);
